98ca0f53ff466a8fb019f102af4a1b42784316e8
AAM <punktiktok@gmail.com>
2025-10-05 01:04:05 +0200

    PerimetroDiCirconferenza

---
+++ perimetrodicirconferenza/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+#define PI 3.1415926535
+
+int main (){
+    int raggio, area, perimetro;
+    scanf("%d", & raggio);
+    raggio=sqrt(area/PI);
+    perimetro=2*PI*raggio;
+    printf("Il perimetro e %d\n", perimetro);
+    return 0;
+}
