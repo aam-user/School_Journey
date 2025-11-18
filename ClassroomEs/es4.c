8b83b6291ed3aa92b5c36661de7205f5e5feb937
aam-linux <mahiabdullah668@gmail.com>
2025-11-18 21:04:38 +0100

    risolto qualche errori

--- ClassroomEs/es4.c
+++ ClassroomEs/es4.c
@@ -9,7 +9,7 @@ int main (){
     int numc = (rand() % (max - min + 1)) + min;
     int n,tent;
     tent=0;
-    printf("Scrivi un numero e prova ad indovinare\n");
+    printf("Scrivi un numero e prova ad indovinare e va da 1 a 100\n");
     do
     {
         scanf("%d", &n);
@@ -17,7 +17,7 @@ int main (){
         {
             printf("Numero e troppo grande\n");
             tent++;
-        } else {
+        } else if (n < numc) {
             printf("Numero e troppo piccolo\n");
             tent++;
         }
