839d01b2a87d6adeb47e0ef87a8d6b575e3f802d
aam <mahiabdullah668@gmail.com>
2025-11-25 20:27:44 +0100

    info added

---
+++ Info/pre_post_increment.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 1;
+    // printf("\n%d", ++i);
+    //  printf("\n%d", i);
+    //  print ==> 2; 2;
+
+    // printf("\n%d", i++);
+    //  printf("\n%d", i);
+    //  print ==> 1; 2;
+}
